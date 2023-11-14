7087fd692aa31ae15af6fca33e6ca432438aa142
Filip Hupka <filip.hupka@gmail.com>
2023-11-15 00:05:34 +0100

    debug

--- src/config/passport.js
+++ src/config/passport.js
@@ -3,13 +3,14 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // eslint-disable-next-line space-before-function-paren, func-names
 module.exports = function(passport) {
   passport.use(new GoogleStrategy(
+    console.log("Configuring Google strategy for passport"),
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     (accessToken, refreshToken, profile, done) => {
-    // Authentication logic here
+      console.log("Google strategy callback executed"); // Add for debugging
       done(null, profile);
     },
   ));
