8613f4cccaa7741256f4889608b908c55c4a3f35
Filip Hupka <filip.hupka@gmail.com>
2023-11-15 16:11:13 +0100

    Add additional origin to corsOptions.js

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: [process.env.CORS_ORIGIN || 'http://cgm.filiphupka.com', 'http://localhost:3001'],
+  origin: [process.env.CORS_ORIGIN || 'http://cgm.filiphupka.com', 'https://cgm.filiphupka.com', 'http://localhost:3001'],
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
