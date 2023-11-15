d889a2f2e5c5b67c4947504d7782d28953e50e1c
Filip Hupka <filip.hupka@gmail.com>
2023-11-15 02:30:06 +0100

    Add localhost to allowed CORS origins

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || 'http://cgm.filiphupka.com',
+  origin: [process.env.CORS_ORIGIN || 'http://cgm.filiphupka.com', 'http://localhost:3000'],
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
