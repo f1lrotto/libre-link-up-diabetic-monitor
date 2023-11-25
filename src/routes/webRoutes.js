4714ce5693d4c2147a3a2560ce051b49368ba99e
Filip Hupka <filip.hupka@gmail.com>
2023-11-25 04:27:23 +0100

    Update webRoutes.js with new routes

--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -5,10 +5,13 @@ const router = express.Router();
 const libreController = require('../controller/libreController');
 
 // GET routes
-router.get('/latest', libreController.getReadingLatestWeb);
-router.get('/last-hours-6', libreController.getReadings6HoursWeb);
-router.get('/last-hours-12', libreController.getReadings12HoursWeb);
-router.get('/last-hours-24', libreController.getReadings24HoursWeb);
-router.get('/last-week', libreController.getReadingsWeekWeb);
+router.get('/latest', libreController.getReadingLatest);
+router.get('/last-hours-6', libreController.getReadings6Hours);
+router.get('/last-hours-12', libreController.getReadings12Hours);
+router.get('/last-hours-24', libreController.getReadings24Hours);
+router.get('/last-week', libreController.getReadingsWeek);
+router.post('/log-meal', libreController.logMeal);
+router.delete('/delete-meal/:id', libreController.deleteMeal);
+router.get('/meals-three-months', libreController.getMealsThreeMonths);
 
 module.exports = router;
