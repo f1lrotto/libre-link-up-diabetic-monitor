261e0cd05f6ab6853dfbe3ccea4a4f7f95a7c9a0
Filip Hupka <filip.hupka@gmail.com>
2023-11-25 04:27:09 +0100

    Add cronjob to update meal post glucose

--- src/controller/cron_controller.js
+++ src/controller/cron_controller.js
@@ -5,11 +5,16 @@ const libreController = require('./libreController');
 const setCronjobs = (CRON_ENABLED) => {
   if (CRON_ENABLED === 'false') {
     console.info('Cronjobs disabled.');
+    cron.schedule('*/1 * * * *', () => {
+      console.info('Running cronjob to save latest reading...');
+      libreController.updateMealPostGlucose();
+    });
     return;
   }
   cron.schedule('*/1 * * * *', () => {
     console.info('Running cronjob to save latest reading...');
     libreController.saveLatestReading();
+    libreController.updateMealPostGlucose();
   });
   cron.schedule('0 * * * *', () => {
     console.info('Running cronjob to save backup data...');
